5a99b6ce2e2c40bd39cfe8f3efa5b79fe2741da6
CrioUser <crio-user@criodo.com>
2022-10-28 08:30:44 +0530

    Keep needed files only

--- scripts/meminfo.sh
+++
@@ -1,6 +0,0 @@
-FILE=/proc/meminfo
-
-MEM_TOTAL=$(grep "MemTotal" /proc/meminfo | awk '{print $2}')
-MEM_FREE=$(grep "MemFree" /proc/meminfo | awk '{print $2}')
-
-echo "$MEM_FREE KB memory free out of $MEM_TOTAL KB"
